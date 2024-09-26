a1d13d618f74d1120a9240404179b26344abad1e
stammath3 <stam.math.12345@gmail.com>
2024-09-26 17:19:46 +0300

    Added content projection for a component inside an other component and created a custom component with an attribute selector to extend a build-in element -> button.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { ServerTrafficComponent } from "./dashboard/server-traffic/server-traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerTrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, ServerTrafficComponent, TicketsComponent, ServerStatusComponent, DashboardItemComponent],
 })
 export class AppComponent {
  
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TicketFormComponent } from "./ticket-form/ticket-form.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [TicketFormComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
