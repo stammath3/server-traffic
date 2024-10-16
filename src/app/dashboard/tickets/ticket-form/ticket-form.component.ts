9653043a3f4b801c4e8e45a58c70320babb83925
stammath3 <stam.math.12345@gmail.com>
2024-10-16 16:57:19 +0300

    Added functinallity for practicing ng-content and content projection, scoping, view encapsulation and host elements.

--- src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
+++ src/app/dashboard/tickets/ticket-form/ticket-form.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-ticket-form',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, ControlComponent],
   templateUrl: './ticket-form.component.html',
   styleUrl: './ticket-form.component.css'
 })
